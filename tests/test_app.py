3b6f5f955ed16d371c6d158aad8923ec43f17327
ChimaOH <cogundipe@outlook.com>
2023-06-02 14:41:48 +0100

    Add message method

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,3 +7,8 @@ def test_get_goodbye(web_client):
     response = web_client.get("/goodbye")
     assert response.status_code == 200
     assert response.data.decode("utf-8") == "Farewell, to all the earthly remains~"
+
+def test_get_specific_message(web_client):
+    response = web_client.get("/message?message=I can drink, I can feast. There's beauty in your beast.")
+    assert response.status_code == 200
+    assert response.data.decode("utf-8") == "I can drink, I can feast. There's beauty in your beast."
