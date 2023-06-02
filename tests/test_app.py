0d3b6a89e44101cd80a7c8bc0b2ae37eafbae1c6
ChimaOH <cogundipe@outlook.com>
2023-06-02 12:04:27 +0100

    Add failing test

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ def test_get_home(web_client):
     assert response.status_code == 200
     assert response.data.decode("utf-8") == "Hello, world!"
 
-def test_post_home(web_client):
-    response = web_client.post("/")
+def test_get_goodbye(web_client):
+    response = web_client.get("/goodbye")
     assert response.status_code == 200
-    assert response.data.decode("utf-8") == "Hello, world!"
+    assert response.data.decode("utf-8") == "Goodbye, world!"
